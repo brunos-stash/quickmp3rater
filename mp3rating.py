98dff97841e13d99f292b942fe4c7094446f559d
brunos-stash <topkekmp4@gmail.com>
2019-09-14 09:20:29 +0200

    added button functionality

--- mp3rating.py
+++ mp3rating.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import re
 import vlc
 
+from overlay import PlayerButtons
 app = Tk()
 app.geometry("300x300")
 app.minsize(width=300,height=300)
@@ -18,11 +19,17 @@ class FinderBox(Listbox):
     def __init__(self, master=None, **kw):
         super().__init__(master=master, **kw)
         self.pack(expand=True, fill='both')
+        self.bind('<Return>', func=self.play)
+        self.bind('<Left>', func=self.prev)
+        self.bind('<Right>', func=self.next)
+        # self.bind('<Return>', func=self.play)
+        self.current_mp3id = 3
         # self.selectpath()
     
     def selectpath(self):
         d = filedialog.askdirectory()
         self.searchpath = Path(d)
+        self.search()
     
     def search(self):
         results = self.searchpath.glob('*.mp3')
@@ -35,33 +42,58 @@ class FinderBox(Listbox):
             self.yview(END)
             self.update()
     
-    def play(self):
-        f = self.get(0)
+    def _mp3_as_uri(self):
+        f = self.get(self.current_mp3id)
         fp = Path(f)
-        furi = fp.as_uri()
+        self.current_mp3uri = fp.as_uri()
         print(fp)
         print(type(fp))
-        print(furi)
-        try:
-            self.mp3
-        except:
-            self.mp3 = vlc.MediaPlayer(furi)
+        print(self.current_mp3uri)
+
+    def play(self, *event):
+        mp3id = self.current_mp3id
+        next_mp3 = self.next_mp3id
+        print(mp3id, next_mp3)
+        if mp3id != next_mp3:
+            
+        self._mp3_as_uri()
+        uri = self.current_mp3uri
+        self.mp3 = vlc.MediaPlayer(uri)
         if self.mp3.is_playing():
             self.mp3.stop()
         else:
             self.mp3.play()
+
+    def next(self, *event):
+        id = self.current_mp3id
+        if self.mp3:
+            self.mp3.stop()
+        id += 1
+        if id > (self.size()-1):
+            self.next_mp3id = 0
+        # self._mp3_as_uri()
+        # uri = self.current_mp3uri
+        # self.mp3 = vlc.MediaPlayer(uri)
+        self.play()
+    
+    def prev(self, *event):
+        if self.mp3:
+            self.mp3.stop()
+        self.current_mp3id -= 1
+        if self.current_mp3id < 0:
+            self.current_mp3id = self.size()-1
+        self.play()
+
 
 class FindBtn(Button):
     def __init__(self, master=None, **kw):
         super().__init__(master=master, **kw)
         self.pack()
 
+
+
 mf = MainFrame(master=app)
-b1 = Button(master=mf)
 lb = FinderBox(master=mf,yscrollcommand=True)
-b1 = FindBtn(master=mf, text='Select path', command=lb.selectpath)
-b2 = FindBtn(master=mf, text='Start search', command=lb.search)
-b2 = FindBtn(master=mf, text='Play', command=lb.play)
-# mf.pack(expand=True)
-# lb.pack(expand=True)
+b1 = Button(master=mf, text='select',command=lb.selectpath)
+b1.pack()
 app.mainloop()
